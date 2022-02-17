876ea8157de6c2b14afd00b75b3fae7163fdc8ab
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-02-16 22:51:09 -0500

    Added game over function

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -109,6 +109,7 @@ document.addEventListener('DOMContentLoaded', () => {
             draw();
             displayShape();
             addScore();
+            gameOver();
         }
     }
 
@@ -212,6 +213,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 squares = squaresRemoved.concat(squares)
                 squares.forEach(cell => grid.appendChild(cell))
             }
+        }
+    }
+
+    //game over
+    function gameOver() {
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            scoreDisplay.innerHTML = 'end';
+            clearInterval(timerId);
         }
     }
 
