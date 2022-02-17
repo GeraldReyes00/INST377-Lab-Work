80441cc85788005048ca1466a66086e200869838
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-02-16 20:43:05 -0500

    Pieces can now move left, right, and down

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentRotation = 0;
 
     //randomly select a Tetromino and its first rotation
-    let random = Math.floor(Math.random()*theTetrominoes.length);
+    let random = Math.floor(Math.random() * theTetrominoes.length);
     let current = theTetrominoes[random][currentRotation];
 
     //draw the Tetromino
@@ -59,38 +59,82 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-   function undraw() {
-       current.forEach(index => {
-           squares[currentPosition + index].classList.remove('tetromino')
-       })
-   }
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
 
-   //make the tetromino move down every second
-   timerId = setInterval(moveDown, 1000);
+    //make the tetromino move down every second
+    timerId = setInterval(moveDown, 1000);
 
-   //move down function
-   function moveDown () {
+    //assign functions to keyCodes
+    function control(e) {
+        if (e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            //rotate
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
 
-    undraw();
-    currentPosition += width;
-    draw();
-    freeze();
-   }
+    document.addEventListener('keyup', control);
 
-   //freeze function
+    //move down function
+    function moveDown() {
 
-   function freeze() {
-       if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-           current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
+    }
 
-           //start a new tetromino falling
-           random = Math.floor(Math.random() * theTetrominoes.length)
-           current = theTetrominoes[random][currentRotation]
-           currentPosition = 4
-           draw();
+    //freeze function
 
-       }
-   }
+    function freeze() {
+        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+
+            //start a new tetromino falling
+            random = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw();
+        }
+    }
+
+    //move the tetromino left, unless is at the edge or there is a blockage
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if (!isAtLeftEdge) {
+            currentPosition -= 1
+        }
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+
+        draw();
+    }
+
+    //move the tetromino right, unless at the edge or there is a blockage
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if (!isAtRightEdge) {
+            currentPosition += 1
+        }
+
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1;
+        }
+        draw();
+    }
 
 
 })
