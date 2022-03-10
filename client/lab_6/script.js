4d56dc6fd993945dd769acef5a7e1c4107c25d6a
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-03-10 14:49:04 -0500

    Step 11, added if statement and button block style

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -6,19 +6,30 @@ function dataHandler(dataArray) {
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
 
-  //Select submit button and set display to none
+  //Select submit button
   const submit = document.querySelector('.submit_button');
+
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+
+  //Set submit button display to none
   submit.style.setProperty('display', 'none')
+
+
+  //If statement that wraps form listener
+  if (arrayFromJson.data.length > 0) {
+
+    //Set submit button style to block
+    submit.style.setProperty('display', 'block')
 
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.table(arrayFromJson.data); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it contains all 1,000 records we need
   });
+}
 
   dataHandler();
 }
