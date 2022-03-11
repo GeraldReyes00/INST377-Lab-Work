3b791c9627929a3bc838aa4d59348a57d13ff50c
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-03-10 23:01:36 -0500

    Fixed style errors and added list heading

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,49 +1,44 @@
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  // The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
 }
-
 
 function dataHandler(dataArray) {
   console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
   range.forEach((item) => {
     console.log('range item', item);
-  })
+  });
 }
-
 
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
 
-  //Select submit button
+  // Select submit button
   const submit = document.querySelector('.submit_button');
+
+  // Set submit button display to none
+  submit.style.setProperty('display', 'none');
 
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
 
-  //Set submit button display to none
-  submit.style.setProperty('display', 'none')
-
-
-  //If statement that wraps form listener
+  // If statement that wraps form listener
   if (arrayFromJson.data.length > 0) {
+    // Set submit button style to block
+    submit.style.setProperty('display', 'block');
 
-    //Set submit button style to block
-    submit.style.setProperty('display', 'block')
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
 
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-
-    dataHandler(arrayFromJson.data);
-  });
-}
-
-  
+      dataHandler(arrayFromJson.data);
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
